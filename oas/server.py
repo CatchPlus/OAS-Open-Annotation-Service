889f5e65e996f0283e8131e3b12dffb38e04eb4e
Seecr <development@seecr.nl>
2012-01-27 17:04:17 +0100

    JJ: wip

--- oas/server.py
+++ oas/server.py
@@ -1,10 +1,11 @@
 from os.path import join, basename, dirname
 from sys import stdout
+from StringIO import StringIO
 
 from meresco.core import Observable, be, TransactionScope
 
 from meresco.components import readConfig, StorageComponent, Amara2Lxml, XmlPrintLxml, Xml2Fields, Venturi
-from meresco.components.http import ObservableHttpServer, StringServer, BasicHttpHandler, PathFilter
+from meresco.components.http import ObservableHttpServer, StringServer, BasicHttpHandler, PathFilter, PathRename, FileServer
 from meresco.components.http.utils import ContentTypePlainText
 from meresco.components.sru import SruParser, SruHandler, SRURecordUpdate
 
@@ -12,16 +13,18 @@ from meresco.solr.solrinterface import SolrInterface
 from meresco.solr.cql2solrlucenequery import CQL2SolrLuceneQuery
 from meresco.solr.fields2solrdoc import Fields2SolrDoc
 
+from uuid import uuid4
 from weightless.io import Reactor
 from dynamichtml import DynamicHtml
 
 from oas import VERSION_STRING
-from namespaces import namespaces
+from namespaces import namespaces, xpath
 
 ALL_FIELD = '__all__'
 unqualifiedTermFields = [(ALL_FIELD, 1.0)]
 
 dynamicHtmlFilePath = join(dirname(__file__), "dynamic")
+staticHtmlFilePath = join(dirname(__file__), "static")
 
 def dna(reactor, observableHttpServer, config):
     hostName = config['hostName']
@@ -32,6 +35,27 @@ def dna(reactor, observableHttpServer, config):
 
     solrInterface = SolrInterface(host="localhost", port=solrPortNumber, core="oas")
 
+    uploadHelix =  \
+        (TransactionScope('batch'),
+            (TransactionScope('record'),    
+                (Venturi(
+                    should=[
+                        ('rdf', '/rdf:RDF'),
+                    ],
+                    namespaceMap=namespaces),
+
+                    (XmlPrintLxml(fromKwarg='lxmlNode', toKwarg='data'),
+                        (storageComponent,)
+                    ),  
+                    (Xml2Fields(),
+                        (Fields2SolrDoc(transactionName="record", partname="solr"),
+                            (solrInterface,)
+                        )   
+                    )
+                )
+            )
+        )
+
     return \
         (Observable(),
             (observableHttpServer,
@@ -39,34 +63,29 @@ def dna(reactor, observableHttpServer, config):
                     (PathFilter("/update"),
                         (SRURecordUpdate(),
                             (Amara2Lxml(fromKwarg="amaraNode", toKwarg="lxmlNode"),
-                                (TransactionScope('batch'),
-                                    (TransactionScope('record'),    
-                                        (Venturi(
-                                            should=[
-                                                ('rdf', '/rdf:RDF'),
-                                            ],
-                                            namespaceMap=namespaces),
-
-                                            (XmlPrintLxml(fromKwarg='lxmlNode', toKwarg='data'),
-                                                (storageComponent,)
-                                            ),  
-                                            (Xml2Fields(),
-                                                (Fields2SolrDoc(transactionName="record", partname="solr"),
-                                                    (solrInterface,)
-                                                )   
-                                            )
-                                        )
-                                    )
-                                )
+                                uploadHelix,
                             )   
                         )   
                     ),
-                    (PathFilter("/", excluding=["/info", "/sru", "/update"]),
-                        (DynamicHtml([dynamicHtmlFilePath], reactor=reactor, indexPage='/index'),
+                    (PathFilter("/", excluding=["/info", "/sru", "/update", "/static"]),
+                        (DynamicHtml([dynamicHtmlFilePath], reactor=reactor, 
+                            indexPage='/index', 
+                            additionalGlobals={
+                                'StringIO': StringIO, 
+                                'xpath': xpath,
+                                'uuid': uuid4,
+                                }),
+                            uploadHelix
+                        )
+                    ),
+                    (PathFilter('/static'),
+                        (PathRename(lambda path: path[len('/static'):]),
+                            (FileServer(staticHtmlFilePath),)
                         )
                     ),
                     (PathFilter("/sru"),
-                        (SruParser(host=hostName, port=portNumber, defaultRecordSchema='rdf', defaultRecordPacking='xml'),
+                        (SruParser(host=hostName, port=portNumber, 
+                            defaultRecordSchema='rdf', defaultRecordPacking='xml'),
                             (SruHandler(drilldownSortedByTermCount=True),
                                 (CQL2SolrLuceneQuery(unqualifiedTermFields),
                                     (solrInterface,)
--- test/integration/oastest.py
+++ test/integration/oastest.py
@@ -1,16 +1,45 @@
 from integrationtestcase import IntegrationTestCase
-from utils import getRequest
+from utils import getRequest, postRequest
 from lxml.etree import tostring
+from uuid import uuid4
+from urllib import urlencode
 
 from oas.namespaces import xpath
 
 class OasTest(IntegrationTestCase):
+
+    def assertQuery(self, query, count):
+        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(
+            version="1.1", operation="searchRetrieve", query=query), parse='lxml')
+        self.assertEquals([str(count)], xpath(body, '/srw:searchRetrieveResponse/srw:numberOfRecords/text()'))
 
     def testGetInfo(self):
         headers, body = getRequest(self.portNumber, "/info/version", parse=False)
         self.assertTrue(body.startswith("Open Annotation Service, version: "), body)
 
     def testSru(self):
-        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(version="1.1", operation="searchRetrieve", query='RDF.Description.name = "J. Bloggs"'), parse='lxml')
-        self.assertEquals(["1"], xpath(body, '/srw:searchRetrieveResponse/srw:numberOfRecords/text()'))
+        self.assertQuery('RDF.Description.name = "J. Bloggs"', 1)
+        self.assertQuery('Bloggs', 1)
 
+    def testPostAnnotation(self):
+        identifier = "urn:uuid:%s" % uuid4()
+        annotationBody = """<rdf:RDF 
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" 
+    xmlns:oac="http://www.openannotation.org/ns/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:dcterms="http://purl.org/dc/terms/"
+    xmlns:foaf="http://xmlns.com/foaf/0.1/">
+
+    <rdf:Description about="%(identifier)s">
+        <rdf:type rdf:resource="http://www.openannotation.org/ns/Annotation"/>
+        <oac:hasBody rdf:resource="ex:HDFI-1"/>
+        <oac:hasTarget rdf:resource="ex:HDFV"/>
+        <dc:title>An Annotions submitted through a form</dc:title>
+        <dcterms:creator rdf:resource="ex:User"/>
+        <dcterms:created>2010-02-01 12:34:56</dcterms:created>
+    </rdf:Description>
+</rdf:RDF>""" % locals()
+        self.assertQuery('RDF.Description.title = "An Annotions submitted through a form"', 0)
+
+        postRequest(self.portNumber, '/uploadform', urlencode(dict(annotation=annotationBody)))
+        self.assertQuery('RDF.Description.title = "An Annotions submitted through a form"', 1)
