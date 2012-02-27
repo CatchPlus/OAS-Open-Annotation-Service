42b43770bc3089efa1a548664ed3c50d5de2ae85
Seecr <development@seecr.nl>
2012-02-27 11:53:11 +0100

    JJ: added test and example data

--- test/publishtest.py
+++ test/publishtest.py
@@ -10,24 +10,39 @@ from oas import Publish
 
 class PublishTest(SeecrTestCase):
 
-    def testOne(self):
-        observer = CallTrace(emptyGeneratorMethods=['add'])
-        dna = be(
+    def setUp(self):
+        SeecrTestCase.setUp(self)
+        self.observer = CallTrace(emptyGeneratorMethods=['add'])
+        self.dna = be(
             (Observable(),
                 (Publish(baseUrl="http://some.where/here"),
-                    (observer, )
+                    (self.observer, )
                 )
             )
         )
+
+    def testOne(self):
         xml="""<rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:oac="http://www.openannotation.org/ns/">
     <oac:Annotation rdf:about="urn:id:1"/>
 </rdf:RDF>"""
-        list(compose(dna.all.process(parse(StringIO(xml)))))
+        list(compose(self.dna.all.process(parse(StringIO(xml)))))
         self.assertEqualsWS("""<rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:oac="http://www.openannotation.org/ns/">
     <oac:Annotation rdf:about="http://some.where/here/urn%3Aid%3A1"><dc:identifier xmlns:dc="http://purl.org/dc/elements/1.1/">urn:id:1</dc:identifier></oac:Annotation>
-</rdf:RDF>""", tostring(observer.calledMethods[0].kwargs['lxmlNode']))
-        self.assertEquals("http://some.where/here/urn%3Aid%3A1", observer.calledMethods[0].kwargs['identifier'])
+</rdf:RDF>""", tostring(self.observer.calledMethods[0].kwargs['lxmlNode']))
+        self.assertEquals("http://some.where/here/urn%3Aid%3A1", self.observer.calledMethods[0].kwargs['identifier'])
+
+
+    def testDontPublishIfAlreadyAnURL(self):
+        xml="""<rdf:RDF
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:oac="http://www.openannotation.org/ns/">
+    <oac:Annotation rdf:about="http://some.where.else/1"/>
+</rdf:RDF>"""
+        list(compose(self.dna.all.process(parse(StringIO(xml)))))
+        self.assertEqualsWS(xml, tostring(self.observer.calledMethods[0].kwargs['lxmlNode']))
+        self.assertEquals("http://some.where.else/1", self.observer.calledMethods[0].kwargs['identifier'])
+
