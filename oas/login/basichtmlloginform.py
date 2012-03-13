04f561cc8f0d7cb938d22e674732152d97b3aa20
Seecr <development@seecr.nl>
2012-03-13 15:46:07 +0100

    TJ: show errors.

--- oas/login/basichtmlloginform.py
+++ oas/login/basichtmlloginform.py
@@ -2,6 +2,7 @@
 from meresco.core import Observable
 from meresco.components.http.utils import CRLF
 from cgi import parse_qs
+from xml.sax.saxutils import quoteattr, escape as xmlEscape
 
 class BasicHtmlLoginForm(Observable):
     def __init__(self, action, page, name=None):
@@ -16,22 +17,30 @@ class BasicHtmlLoginForm(Observable):
             password = bodyArgs.get('password', [None])[0]
             if self.call.validateUser(username=username, password=password):
                 session['user'] = User(username)
+            else:
+                session['BasicHtmlLoginForm.formValues']={'username': username, 'errorMessage': 'Invalid username or password'}
+
 
         yield "HTTP/1.0 302 Found"+CRLF+"Location: %s" % self._page
         yield 2*CRLF
 
-    def loginForm(self):
-        yield """<div id="login">
-    <form method="POST" action="%s">
+    def loginForm(self, session=None, **kwargs):
+        formValues = session.get('BasicHtmlLoginForm.formValues', {}) if session else {}
+        yield """<div id="login">\n"""
+        if 'errorMessage' in formValues:
+            yield '    <p class="error">%s</p>\n' % xmlEscape(formValues['errorMessage'])
+        action = self._action
+        usernameValue = '' if 'username' not in formValues else " value=%s" % quoteattr(formValues['username'])
+        yield """    <form method="POST" action="%(action)s">
         <dl>
             <dt>Username</dt>
-            <dd><input type="text" name="username"/></dd>
+            <dd><input type="text" name="username"%(usernameValue)s/></dd>
             <dt>Password</dt>
             <dd><input type="password" name="password"/></dd>
             <dd class="submit"><input type="submit" value="login"/></dd>
         </dl>
     </form>
-</div>""" % self._action
+</div>""" % locals()
 
 class User(object):
     def __init__(self, name):
--- test/login/basichtmlloginformtest.py
+++ test/login/basichtmlloginformtest.py
@@ -60,9 +60,32 @@ class BasicHtmlLoginFormTest(SeecrTestCase):
         result = ''.join(form.handleRequest(path='/login', Client=('127.0.0.1', 3451), method='POST', Body=Body, session=session))
 
         self.assertFalse('user' in session)
+        self.assertEquals({'username':'user', 'errorMessage': 'Invalid username or password'}, session['BasicHtmlLoginForm.formValues'])
         header, body = result.split(CRLF*2)
         self.assertTrue('302' in header)
         self.assertTrue('Location: /home' in header)
 
         self.assertEquals(['validateUser'], [m.name for m in observer.calledMethods])
         self.assertEquals({'username': 'user', 'password':'wrong'}, observer.calledMethods[0].kwargs)
+
+
+    def testLoginFormWithError(self):
+        form = BasicHtmlLoginForm(action='/login', page='/home') 
+        session = {}
+        session['BasicHtmlLoginForm.formValues']={'username': '<us"er>', 'errorMessage': 'Invalid <username> or "password"'}
+        result = ''.join(form.loginForm(session=session))  
+
+        self.assertEquals("""<div id="login">
+    <p class="error">Invalid &lt;username&gt; or "password"</p>
+    <form method="POST" action="/login">
+        <dl>
+            <dt>Username</dt>
+            <dd><input type="text" name="username" value='&lt;us"er&gt;'/></dd>
+            <dt>Password</dt>
+            <dd><input type="password" name="password"/></dd>
+            <dd class="submit"><input type="submit" value="login"/></dd>
+        </dl>
+    </form>
+</div>""", result)
+
+
