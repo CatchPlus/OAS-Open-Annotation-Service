62d94b188b981d108a60cca8ebd39e7a8a5967e3
Seecr <development@seecr.nl>
2012-06-28 14:11:08 +0200

    HM/JJ: added more testcases

--- test/integration/inlinetest.py
+++ test/integration/inlinetest.py
@@ -31,8 +31,6 @@ from oas.namespaces import xpath, getAttrib
 from seecr.test.integrationtestcase import IntegrationTestCase
 from seecr.test.utils import getRequest, postRequest
 
-
-
 class InlineTest(IntegrationTestCase):
     def testEntityAnnotation(self):
         query = "urn:id:ea:1"
@@ -48,3 +46,44 @@ class InlineTest(IntegrationTestCase):
         self.assertEquals(['Dit is een beschrijving van Den Haag. En dit is een tweede zin.'], xpath(ea_1, 'oac:Annotation/oac:hasTarget/oac:ConstrainedTarget[dc:identifier/text()="urn:id:ct:4"]/oac:constrains/cnt:ContentAsText[@rdf:about="urn:id:ib:0"]/cnt:chars/text()'))
 
         self.assertEquals(["UTF-8"], xpath(ea_1, 'oac:Annotation/oac:hasTarget/oac:ConstrainedTarget[dc:identifier/text()="urn:id:ct:3"]/oac:constrainedBy/oac:Constraint[@rdf:about="urn:id:c:5"]/cnt:characterEncoding/text()'))
+
+    def testTextAnnotations_ta0(self):
+        query = "urn:id:ta:0"
+        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(
+            version="1.2", operation="searchRetrieve", query=query), parse='lxml')
+
+        ta_0 = xpath(body, "/srw:searchRetrieveResponse/srw:records/srw:record/srw:recordData/rdf:RDF")[0]
+
+        self.assertEquals(['Dit is een beschrijving van Den Haag. En dit is een tweede zin.'], xpath(ta_0, 'oac:Annotation/oac:hasBody/cnt:ContentAsText[@rdf:about="urn:id:ib:0"]/cnt:chars/text()'))
+        self.assertEquals(['Canvas for imageScan1.jpg'], xpath(ta_0, 'oac:Annotation/oac:hasTarget/dms:Canvas[@rdf:about="http://catchplus.nl/annotation/Canvas1"]/dc:title/text()'))
+        
+    def testTextAnnotations_ta1(self):
+        query = "urn:id:ta:1"
+        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(
+            version="1.2", operation="searchRetrieve", query=query), parse='lxml')
+
+        ta_1 = xpath(body, "/srw:searchRetrieveResponse/srw:records/srw:record/srw:recordData/rdf:RDF")[0]
+
+        self.assertEquals(['Dit is een beschrijving van Den Haag. En dit is een tweede zin.'], xpath(ta_1, 'oac:Annotation/oac:hasBody/oac:ConstrainedBody[@rdf:about="urn:id:cb:1"]/oac:constrains/cnt:ContentAsText[@rdf:about="urn:id:ib:0"]/cnt:chars/text()'))
+        self.assertEquals(['Canvas for imageScan1.jpg'], xpath(ta_1, 'oac:Annotation/oac:hasTarget/oac:ConstrainedTarget[dc:identifier/text()="urn:id:ct:1"]/oac:constrains/dms:Canvas[@rdf:about="http://catchplus.nl/annotation/Canvas1"]/dc:title/text()'))
+        
+    def testTextAnnotations_ta2(self):
+        query = "urn:id:ta:2"
+        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(
+            version="1.2", operation="searchRetrieve", query=query), parse='lxml')
+
+        ta_2 = xpath(body, "/srw:searchRetrieveResponse/srw:records/srw:record/srw:recordData/rdf:RDF")[0]
+
+        self.assertEquals(['Dit is een beschrijving van Den Haag. En dit is een tweede zin.'], xpath(ta_2, 'oac:Annotation/oac:hasBody/oac:ConstrainedBody[@rdf:about="urn:id:cb:2"]/oac:constrains/cnt:ContentAsText[@rdf:about="urn:id:ib:0"]/cnt:chars/text()'))
+        self.assertEquals(['Canvas for imageScan1.jpg'], xpath(ta_2, 'oac:Annotation/oac:hasTarget/oac:ConstrainedTarget[dc:identifier/text()="urn:id:ct:2"]/oac:constrains/dms:Canvas[@rdf:about="http://catchplus.nl/annotation/Canvas1"]/dc:title/text()'))
+        
+    def testImageAnnotations_ia1(self):
+        query = "urn:id:ia:1"
+        headers, body = getRequest(self.portNumber, "/sru", arguments=dict(
+            version="1.2", operation="searchRetrieve", query=query), parse='lxml')
+
+        ia_1 = xpath(body, "/srw:searchRetrieveResponse/srw:records/srw:record/srw:recordData/rdf:RDF")[0]
+
+        self.assertEquals(['http://catchplus.nl/annotation/imageScan1.jpg'], xpath(ia_1, 'oac:Annotation/oac:hasBody/@rdf:resource'))
+        self.assertEquals(['Canvas for imageScan1.jpg'], xpath(ia_1, 'oac:Annotation/oac:hasTarget/dms:Canvas[@rdf:about="http://catchplus.nl/annotation/Canvas1"]/dc:title/text()'))
+        
